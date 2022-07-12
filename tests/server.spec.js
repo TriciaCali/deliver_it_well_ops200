ca6915298e61dbba05350c8b25fc1b9399d310fb
Latricia Hunter <latricia@Latricias-iMac.lan>
2022-07-12 15:54:49 -0700

    testing

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -43,7 +43,7 @@ describe('server/app.js', function() {
       .get('/')
       .end((err, res) => {
         expect(err).not.exist;
-        expect(JSON.stringify(res.text)).to.contain('looking forward to going tomorrow');
+        expect(JSON.stringify(res.text)).to.contain('Hey');
         done();
       });
     });
